e99c79802f7568015c1b760740591919d513d8bb
Mert Özen <mert.ozen@gmail.com>
2018-09-22 22:51:40 +0300

    Youtube Api Sample Project Initial

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'API_KEY';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos = {this.state.videos}  />
+      </div>);
+  }
+}
+
+
+ReactDom.render(<App />, document.querySelector('.container'));
