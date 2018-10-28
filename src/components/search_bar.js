de1d9dcd4df04d19cf227d57b89ea510902b823f
Mert Özen <mert.ozen@gmail.com>
2018-10-28 17:57:26 +0300

    Youtube clone is done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,19 +7,26 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            term: 'Hi!'
+            term: ''
         };
     }
 
     render() {
-        return ( 
-        <div>
-            <input 
-            value={this.state.term}
-            onChange = {
-                event => this.setState({term: event.target.value})}/> 
-            </div>   
+        return (<div className="search-bar" >
+            <input value={
+                this.state.term
+            }
+                onChange={
+                    event => this.onInputChange(event.target.value)
+                } /> </div >
         );
+    }
+
+    onInputChange(term) {
+        this.setState({
+            term
+        });
+        this.props.onSearchTermChange(term);
     }
 }
 
